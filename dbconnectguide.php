1fa8d9b7bb2ef63f094a2eed8d1eed122ca8f006
MatthewJLowman <114599804+MatthewJLowman@users.noreply.github.com>
2022-11-15 11:13:43 -0500

    Add files via upload

---
+++ dbconnectguide.php
@@ -0,0 +1,17 @@
+<?php
+// local
+$dbhost = "localhost";
+$dbuser = "uzggbbcvd2mxm";
+$dbpass = "){$18121rf#H";
+$dbname = "salamanders";
+
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+mysqli_close($connection);
+
+
+//a2
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "charli12_doall");
+// define("DB_PASS", "ErYje7HV2o2Lqp7B9gx");
+// define("DB_NAME", "charli12_salamanders");
